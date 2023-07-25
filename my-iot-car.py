e41f735e7da5724b91608ef4467e22e2eb5312cb
Eric <eric_h_y@hotmail.com>
2023-07-25 00:05:14 -0300

    Read firebase data change by polling and update car movement (direction and speed)

--- my-iot-car.py
+++ my-iot-car.py
@@ -13,6 +13,11 @@ from time import sleep
 
 Sentry = True
 
+car_motor1 = None
+car_motor2 = None
+car_direction = None
+car_speed = None
+
 #---------------------------------------------------------------------------------------------------
 # Functions
 #---------------------------------------------------------------------------------------------------
@@ -22,19 +27,54 @@ def SignalHandler_SIGINT(SignalNumber,Frame):
 	global Sentry
 	Sentry = False
 
+def update_car_movement():
+	global car_motor1
+	global car_motor2
+	global car_direction
+	global car_speed
+	if car_direction:
+		car_motor1.forward(car_speed)
+		car_motor2.forward(car_speed)
+	else:
+		car_motor1.backward(car_speed)
+		car_motor2.backward(car_speed)
+
+def assert_speed(speed):
+	if speed >= 0.0 and speed <= 1.0:
+		return True
+	else:
+		return False
+
 #---------------------------------------------------------------------------------------------------
 # Code initialization
 #---------------------------------------------------------------------------------------------------
 
 signal.signal(signal.SIGINT, SignalHandler_SIGINT)
+
 try:
 	exec(open("private-firebase-config.py").read())
 except FileNotFoundError:
 	print("Error: File 'private-firebase-config.py' not found")
 	sys.exit(-1)
 firebase = pyrebase.initialize_app(config)
-motor1 = Motor(6, 13)
-motor2 = Motor(19, 26)
+car_motor1 = Motor(6, 13)
+car_motor2 = Motor(19, 26)
+
+database = firebase.database()
+car_direction = database.child("forward").get().val()
+car_speed = database.child("speed").get().val()
+if not assert_speed(car_speed):
+	car_speed = 0.0
+	print("Invalid car speed from database. Speed set to zero")
+print("Car initial parameters:")
+if car_direction:
+	print("Direction: forward")
+else:
+	print("Direction: backward")
+print("Speed: ", str(car_speed))
+
+update_car_movement()
+
 print("Process started. Press CTRL+C to exit")
 
 #---------------------------------------------------------------------------------------------------
@@ -43,16 +83,31 @@ print("Process started. Press CTRL+C to exit")
 
 while Sentry:
 	timestamp_ini = datetime.datetime.now()
+	
 	database = firebase.database()
-	direction = database.child("forward").get().val()
-	if direction:
-		print("Andando para frente")
-	else:
-		print("Andando para trás")
+
+	new_direction = database.child("forward").get().val()
+	if new_direction != car_direction:
+		car_direction = new_direction
+		if car_direction:
+			print("Direction changed to forward")
+		else:
+			print("Direction changed to backward")
+		update_car_movement()
+
+	new_speed = database.child("speed").get().val()
+	if new_speed != car_speed:
+		if assert_speed(new_speed):
+			car_speed = new_speed
+			print("Speed changed to ", str(car_speed))
+			update_car_movement()
+		else:
+			print("Invalid car speed from database. Speed was not updated")
+
 	timestamp_end = datetime.datetime.now()
 	if (timestamp_end - timestamp_ini).microseconds < 300000:
 		sleep(0.3)
 
 # Finishing execution
-motor1.close()
-motor2.close()
+car_motor1.close()
+car_motor2.close()
